5dafa53db28b0b43f4e866ecc557965cf09962b1
Stefano Genetti <stefanogenetti2000@gmail.com>
2024-09-07 08:57:28 +0200

    random search execution time and multiple run

--- optimization_random_search.py
+++ optimization_random_search.py
@@ -1,10 +1,13 @@
 from typing import List, Dict, Tuple, Set
 from alpypeopt import AnyLogicModel
-from ast import literal_eval
+from datetime import datetime
 import matplotlib.pyplot as plt
 import csv
 import argparse
+import os
+import pandas as pd
 import random
+import time
 
 def read_arguments():
     parser = argparse.ArgumentParser(description="ABCD production optimizer.")
@@ -12,6 +15,8 @@ def read_arguments():
     parser.add_argument("--random_seed", type=int, default=42, help="Seed to initialize the pseudo-random number generation.")
     parser.add_argument("--input_csv", type=str, help="File path of the CSV where the optimization output has been stored.")
     parser.add_argument("--n_trials", type=int, default=100, help="Number of optimization trials.")
+    parser.add_argument("--out_dir", type=str, help="Output folder.")
+    parser.add_argument('--no_runs', type=int, default=1, help='Number of runs.')
 
     args = parser.parse_args()
     args = vars(args)
@@ -22,30 +27,32 @@ if __name__ == '__main__':
     args = read_arguments()
     rng = random.Random(args["random_seed"])
 
-    # history of evaluations
-    history_x:List[List[int]] = []
-    history_y:List[int] = []
-
     if args["input_csv"]:
         csv_file_path = args["input_csv"]
-        csv_file = open(csv_file_path, mode='r', newline='')
-        csv_reader = csv.reader(csv_file)
-        next(csv_reader)    # skip the header
-        for row in csv_reader:
-            x = literal_eval(row[0])  # convert the string representation of list back to a list
-            y = int(row[1])           # convert the string representation of integer back to an integer
-            history_x.append(x)
-            history_y.append(y)
-
-        # plot fitness trent
+        df = pd.read_csv(csv_file_path)
         plt.figure(figsize=(10, 6))
-        plt.plot(range(len(history_y)), history_y, marker='o', linestyle='-', color='b')
-        plt.xlabel('Evaluation')
+        plt.plot(range(len(df["y"])), df["y"].cummin(), marker='o', linestyle='-', linewidth=1, markersize=4, label='revenue')
+        plt.xlabel('Trial')
         plt.ylabel('Revenue')
         plt.title('Fitness Trend')
+        plt.legend()
         plt.grid(True)
-        plt.show()
+        #plt.show()
+        plt.savefig(f"{args['out_dir']}/random.pdf")
+        plt.savefig(f"{args['out_dir']}/random.png")
     else:
+        # create directory for saving results
+        current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
+        output_folder_path = f"{args['out_dir']}/{current_datetime}"
+        os.makedirs(output_folder_path)
+
+        # execution time csv file
+        csv_execution_time_file_path = f"{output_folder_path}/exec_random.csv"
+        csv_execution_time_file = open(csv_execution_time_file_path, mode='w', newline='')
+        csv_execution_time_writer = csv.writer(csv_execution_time_file)
+        csv_execution_time_writer.writerow(["run", "time"])
+        csv_execution_time_file.close()
+
         abcd_model = AnyLogicModel(
             env_config={
                 'run_exported_model': False
@@ -73,33 +80,60 @@ if __name__ == '__main__':
             
             return model_output.getTotalRevenue()
 
-        # run the optimizer
-        best_y = -100000
-        best_x = [random.randint(0,1) for _ in range(100)]
-        for _ in range(args["n_trials"]):
-            x = [random.randint(0,1) for _ in range(100)]
-            y = simulation(x)
-            if y>best_y:
-                best_y = y
-                best_x = x.copy()
-            history_x.append(best_x.copy())
-            history_y.append(int(y))
+        for r in range(args["no_runs"]):
+            # create directory for saving results of the run
+            output_folder_run_path = output_folder_path+"/"+str(r+1)
+            os.makedirs(output_folder_run_path)
 
-        # print result
-        print(f"Solution is {best_x} for a value of {best_y}")
+            # write a txt log file with algorithm configuration and other execution details
+            log_file = open(f"{output_folder_run_path}/log.txt", 'w')
+            log_file.write(f"algorithm: random search\n")
+            log_file.write(f"current date and time: {datetime.now().strftime('%Y%m%d_%H%M%S')}\n")
+            log_file.write(f"n_trials: {args['n_trials']}\n")
+            log_file.write(f"no_runs: {args['no_runs']}\n")
+            log_file.write(f"\n===============\n")
+
+            # history of evaluations
+            history_x:List[List[int]] = []
+            history_y:List[int] = []
+
+            # run the optimizer
+            best_y = -100000
+            best_x = [random.randint(0,1) for _ in range(100)]
+            start_time = time.time()
+            for _ in range(args["n_trials"]):
+                x = [random.randint(0,1) for _ in range(100)]
+                y = simulation(x)
+                if y>best_y:
+                    best_y = y
+                    best_x = x.copy()
+                history_x.append(best_x.copy())
+                history_y.append(int(y))
+            execution_time = time.time()-start_time
+            # store execution time of the run
+            csv_execution_time_file = open(csv_execution_time_file_path, mode='a', newline='')
+            csv_execution_time_writer = csv.writer(csv_execution_time_file)
+            csv_execution_time_writer.writerow([r, execution_time])
+            csv_execution_time_file.close()
+
+            # print result
+            print(f"Solution is {best_x} for a value of {best_y}")
+            log_file.write(f"Solution is {best_x} for a value of {best_y}\n")
+
+            # store fitness trend history in csv output file
+            csv_file_path = f"{output_folder_run_path}/history_random.csv"
+            csv_file = open(csv_file_path, mode='w', newline='')
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["x", "y"])
+            for x, y in zip(history_x, history_y):
+                csv_writer.writerow([str(x), y])
+            csv_file.close()
+            print(f"Data successfully written to history_random.csv")
+            log_file.write(f"Data successfully written to history_random.csv\n")
+            log_file.close()
 
         # run simulation with optimal result to use UI to explore results in AnyLogic
-        simulation(best_x, reset=False)
+        #simulation(best_x, reset=False)
 
         # close model
         abcd_model.close()
-
-        # store fitness trend history in csv output file
-        csv_file_path = "history_random_search.csv"
-        csv_file = open(csv_file_path, mode='w', newline='')
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["x", "y"])
-        for x, y in zip(history_x, history_y):
-            csv_writer.writerow([str(x), y])
-        csv_file.close()
-        print(f"Data successfully written to {csv_file_path}")
